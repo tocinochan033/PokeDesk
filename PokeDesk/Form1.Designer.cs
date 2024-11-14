d26686cb258657f61c5b186df78ad53c830fc1f4
Martin Vazquez <vazquezlmartin33@gmail.com>
2024-11-13 21:15:52 -0800

    inyeccion API

--- PokeDesk/Form1.Designer.cs
+++ PokeDesk/Form1.Designer.cs
@@ -28,37 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pokemonPictureBox = new System.Windows.Forms.PictureBox();
+            this.pokemonNameLabel = new System.Windows.Forms.Label();
+            this.pokemonTextBox = new System.Windows.Forms.TextBox();
+            this.pokemonButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // pokemonPictureBox
+            // 
+            this.pokemonPictureBox.BackColor = System.Drawing.Color.White;
+            this.pokemonPictureBox.Location = new System.Drawing.Point(169, 116);
+            this.pokemonPictureBox.Name = "pokemonPictureBox";
+            this.pokemonPictureBox.Size = new System.Drawing.Size(240, 240);
+            this.pokemonPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pokemonPictureBox.TabIndex = 0;
+            this.pokemonPictureBox.TabStop = false;
+            // 
+            // pokemonNameLabel
+            // 
+            this.pokemonNameLabel.AutoSize = true;
+            this.pokemonNameLabel.BackColor = System.Drawing.Color.White;
+            this.pokemonNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pokemonNameLabel.Location = new System.Drawing.Point(88, 317);
+            this.pokemonNameLabel.Name = "pokemonNameLabel";
+            this.pokemonNameLabel.Size = new System.Drawing.Size(93, 39);
+            this.pokemonNameLabel.TabIndex = 1;
+            this.pokemonNameLabel.Text = "5555";
+            // 
+            // pokemonTextBox
+            // 
+            this.pokemonTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pokemonTextBox.Location = new System.Drawing.Point(214, 627);
+            this.pokemonTextBox.Name = "pokemonTextBox";
+            this.pokemonTextBox.Size = new System.Drawing.Size(140, 30);
+            this.pokemonTextBox.TabIndex = 2;
+            this.pokemonTextBox.TextChanged += new System.EventHandler(this.pokemonTextBox_TextChanged);
+            // 
+            // pokemonButton
+            // 
+            this.pokemonButton.AllowDrop = true;
+            this.pokemonButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pokemonButton.Location = new System.Drawing.Point(54, 523);
+            this.pokemonButton.Name = "pokemonButton";
+            this.pokemonButton.Size = new System.Drawing.Size(89, 85);
+            this.pokemonButton.TabIndex = 3;
+            this.pokemonButton.UseVisualStyleBackColor = false;
+            this.pokemonButton.Click += new System.EventHandler(this.pokemonButton_Click);
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(76, 64);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-5, -3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(271, 164);
-            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(402, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1262, 713);
+            this.Controls.Add(this.pokemonButton);
+            this.Controls.Add(this.pokemonTextBox);
+            this.Controls.Add(this.pokemonNameLabel);
+            this.Controls.Add(this.pokemonPictureBox);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -67,8 +110,11 @@
 
         #endregion
 
+        private System.Windows.Forms.PictureBox pokemonPictureBox;
+        private System.Windows.Forms.Label pokemonNameLabel;
+        private System.Windows.Forms.TextBox pokemonTextBox;
+        private System.Windows.Forms.Button pokemonButton;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
     }
 }
 
--- PokeDesk/Form1.cs
+++ PokeDesk/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Runtime.Remoting;
 
 namespace PokeDesk
 {
@@ -15,6 +18,66 @@ namespace PokeDesk
         public Form1()
         {
             InitializeComponent();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private static readonly HttpClient client = new HttpClient();
+        private async void LoadPokemon(string pokemonName)
+        {
+            string url = $"https://pokeapi.co/api/v2/pokemon/{pokemonName.ToLower()}";
+            try
+            {
+                string responseBody = await client.GetStringAsync(url);
+
+                // Deserializa el JSON en un objeto de tipo Pokemon
+                Pokemon pokemonData = JsonConvert.DeserializeObject<Pokemon>(responseBody);
+
+                // Obtén el nombre y la URL de la imagen
+                string name = pokemonData.Name;
+                string imageUrl = pokemonData.Sprites.FrontDefault;
+
+                // Actualiza la interfaz
+                pokemonNameLabel.Text = name;
+                pokemonPictureBox.Load(imageUrl);
+            }
+            catch (HttpRequestException e)
+            {
+                MessageBox.Show("Error al obtener los datos del Pokémon: " + e.Message);
+            }
+        }
+
+        public class Pokemon
+        {
+            public string Name { get; set; }
+            public Sprites Sprites { get; set; }
+        }
+
+        public class Sprites
+        {
+            [JsonProperty("front_default")]
+            public string FrontDefault { get; set; }
+        }
+
+
+
+        private void pokemonTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pokemonButton_Click(object sender, EventArgs e)
+        {
+            string pokename = pokemonTextBox.Text.ToString();
+            LoadPokemon(pokename);
+            pokemonTextBox.Clear();
         }
     }
 }
